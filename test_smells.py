db0af8f0fbcf5706ca8dc21c5c9fff4fcd27c0e9
Aman Mohamed Ali <69119173+spidervirus@users.noreply.github.com>
2026-04-03 16:43:55 +0400

    Add test_smells.py for heuristic verification

---
+++ test_smells.py
@@ -0,0 +1,13 @@
+def bad_code():
+    x = 1000
+    for i in range(10):
+        for j in range(10):
+            for k in range(10):
+                pass
+    # if True:
+    #     print("hello")
+    api_key = "sk-12345"
+    try:
+        1/0
+    except:
+        pass
